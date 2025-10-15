9e8c204a71a513b8f6db0098f0cd535ce108f873
e.ikwukacos101 <5fjyjzd8bf@privaterelay.appleid.com>
2025-10-15 23:23:01 +0100

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let sum = 5550+7310;
+    println!("The addition of 5550 and 7310 is {}",sum);
+
+    let difference:f32 = 95.5-4.3;
+    println!("the difference of 95.5 and 4.3 is{}",difference );
+
+    let product:i32 = 4 * 30;
+    println!("the product of multiplying 4 and 30 is {}",product );
+
+    let quotient:f32 = 56.7/32.2 ;
+    println!("the division on 56.7 by 32.2 is {}",quotient );
+
+    let remainder:i32 =43%5;
+    print!("the remainder of 43 and 5 is {}",remainder );
+}
